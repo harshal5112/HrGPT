78e4b1b9ed1277a100934f1744fcdadb72bfa319
harshal5112 <harshalrasoda@gmail.com>
2025-04-02 03:13:40 +0530

    Initial commit

---
+++ app/src/main/java/np/com/bimalkafle/easybot/Constants.kt
@@ -0,0 +1,7 @@
+package np.com.bimalkafle.easybot
+
+object Constants {
+
+    val apiKey: String
+        get() = "AIzaSyDHvKROrdXO7PeIqYelVbSlFaLLepfmcQw"
+}
---
+++ app/src/main/java/np/com/bimalkafle/easybot/ui/theme/Color.kt
@@ -0,0 +1,16 @@
+package np.com.bimalkafle.easybot.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFF2D2B26)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF211725)
+val PurpleGrey40 = Color(0xFF1E1A23)
+val Pink40 = Color(0xFF7D5260)
+val Black = Color(0xFF0C0C0C)
+
+
+val ColorUserMessage = Color(0xFF545E65)
+val ColorModelMessage = Color(0xFF2D332E)
